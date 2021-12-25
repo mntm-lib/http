b45909cb3bed9b438d99e71b30231c924c249f3f
eolme <im@petrov.engineer>
2021-12-26 01:50:45 +0400

    Allow alias module

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,42 @@ import {
   createServer
 } from './server.js';
 
-export { createServer };
-export default {
-  createServer
+import * as http from 'http';
+
+const patched = {};
+const wildcard = Object.assign(patched, http, {
+  createServer,
+  default: patched
+});
+
+export {
+  createServer,
+  wildcard as default
 };
+
+export {
+  Agent,
+  ClientRequest,
+  IncomingMessage,
+  METHODS,
+  OutgoingMessage,
+  STATUS_CODES,
+  Server,
+  ServerResponse,
+  get,
+  globalAgent,
+  maxHeaderSize,
+  request
+} from 'http';
+
+export type {
+  AgentOptions,
+  ClientRequestArgs,
+  IncomingHttpHeaders,
+  InformationEvent,
+  OutgoingHttpHeader,
+  OutgoingHttpHeaders,
+  RequestListener,
+  RequestOptions,
+  ServerOptions
+} from 'http';
